1d5601121a93f4f50b32f29141c19f9bf1831db2
Ebrahim Sameh <ebrahimsameh16@gmail.com>
2022-12-11 23:52:59 +0200

    Update urls.py

--- bodoo23/user/urls.py
+++ bodoo23/user/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('signup', Signup.as_view(), name='signup'),
     path('user-detail/', UserDetail.as_view(), name='user-detail'),
     path('global-var/', GlobalVarView.as_view(), name='global-var'),
+    path('special-user/', SpecialUserView.as_view(), name='special-user'),
 ]
